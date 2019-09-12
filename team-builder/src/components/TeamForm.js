890a3273a35169a84a379ae44749040e13560227
Your name <a.m.verdine@gmail.com>
2019-09-12 00:30:32 -0400

    Create teammates state array and hook up in App.js

--- team-builder/src/components/TeamForm.js
+++ team-builder/src/components/TeamForm.js
@@ -2,15 +2,29 @@ import React, { useState } from "react";
 
 const TeamForm = () => {
   // console.log(props);
-  const [team, setTeam] = useState();
+  const [team, setTeam] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    exp: "",
+    role: ""  
+  });
 
   return (
     <>
       <div>
-        <form></form>
+        <form>
+          <input  type="text" name="firstname" placeholder="First Name" />
+          <input  type="text" name="lastname" placeholder="Last Name" />
+          <input  type="text" name="email" placeholder="email" />
+          <input  type="text" name="XP" placeholder="Years of Experience" />
+          <input  type="text" name="role" placeholder="Team Role" />
+
+
+        </form>
       </div>
     </>
   );
 };
 
-export default Form;
+export default TeamForm;
