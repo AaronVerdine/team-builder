1fcc4e0b4c49346f57c1ac01332ff666edf68570
Your name <a.m.verdine@gmail.com>
2019-09-11 23:01:01 -0400

    Create Form component and render in App.js

---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  // console.log(props);
+const [team, setTeam] = useState();
+
+  return (
+    <>
+      <div>
+        <form>
+          
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Form;
