0742e75860617dc23bc9ac4a0c70d609a70be68b
Your name <a.m.verdine@gmail.com>
2019-09-12 00:57:37 -0400

    Create state hooks for TeamForm.js and build out form structure

--- team-builder/src/components/TeamForm.js
+++ team-builder/src/components/TeamForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const TeamForm = () => {
+const TeamForm = props => {
   // console.log(props);
   const [team, setTeam] = useState({
     firstName: "",
@@ -10,15 +10,38 @@ const TeamForm = () => {
     role: ""  
   });
 
+  const handleFirstNameChange = event => {
+    setTeam({...setTeam, firstname: event.target.value})
+  }
+  const handleLastNameChange = event => {
+    setTeam({...setTeam, lastname: event.target.value})
+  }
+  const handleEmailChange = event => {
+    setTeam({...setTeam, email: event.target.value})
+  }
+  const handleXPChange = event => {
+    setTeam({...setTeam, exp: event.target.value})
+  }
+  const handleRoleChange = event => {
+    setTeam({...setTeam, role: event.target.value})
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(setTeam);
+  }
+
   return (
     <>
       <div>
-        <form>
-          <input  type="text" name="firstname" placeholder="First Name" />
-          <input  type="text" name="lastname" placeholder="Last Name" />
-          <input  type="text" name="email" placeholder="email" />
-          <input  type="text" name="XP" placeholder="Years of Experience" />
-          <input  type="text" name="role" placeholder="Team Role" />
+        <form onSubmit={handleSubmit}>
+          <input  type="text" name="firstname" placeholder="First Name" onChange={handleFirstNameChange }/><br></br>
+          <input  type="text" name="lastname" placeholder="Last Name" onChange={handleLastNameChange } /><br></br>
+          <input  type="text" name="email" placeholder="email" onChange={handleEmailChange } /><br></br>
+          <input  type="text" name="XP" placeholder="Years of Experience" onChange={handleXPChange} /><br></br>
+          <input  type="text" name="role" placeholder="Team Role" onChange={handleRoleChange} /><br></br>
+          <button type="submit">Register!</button>
+
 
 
         </form>
